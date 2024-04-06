9c6235c5e8856ade93776810bb6a43ea8d7ffec2
Wojciech Wroński <hierodrimm@gmail.com>
2024-04-06 20:58:34 +0200

    Add some changes

--- Assets/Scripts/DontDestroyWhenLoading.cs
+++ Assets/Scripts/DontDestroyWhenLoading.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class DontDestroyWhenLoading : MonoBehaviour
 {
+    private static DontDestroyWhenLoading Instance;
+
     void Awake()
     {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+
         DontDestroyOnLoad(gameObject);
     }
 }
